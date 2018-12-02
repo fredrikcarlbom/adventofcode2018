b0808ce69c9193eaf787d1ed47d4add158e7ecd8
Fredrik Carlbom <fredrik.carlbom@gmail.com>
2018-12-02 13:52:53 +0100

    Day 1 and 2 solutions

---
+++ 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            do {
+                var token = Console.ReadLine();
+                if (!String.IsNullOrWhiteSpace(token)) {
+                    numbers.Add(Int32.Parse(token));
+                } else {
+                    break;
+                }
+            } while (true);
+            Console.WriteLine("Sum:" + numbers.Sum());
+            
+            var sum = 0;
+            var found = false;
+            List<int> allSums = new List<int> { sum };
+            do {
+                foreach (var number in numbers) {
+                    sum += number;
+                    if (!allSums.Exists(item => item == sum)) {
+                        Console.Write(sum + ", ");
+                        allSums.Add(sum);
+                    } else {
+                        Console.WriteLine("BREAKING " + sum);
+                        found = true;
+                        break;
+                    }
+                }
+            } while (!found);
+            allSums.Sort();
+            Console.WriteLine("All Sums: " + String.Join(", ",allSums));
+            Console.WriteLine("First recurring: " + sum);
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = new List<string>();
+            var numberOfThree = 0;
+            var numberOfTwo = 0;
+            var matchedDifferByOne = false;
+            var commonDenominator = "";
+            do {
+                var token = Console.ReadLine();
+                if (!String.IsNullOrWhiteSpace(token)) {
+                    if (!matchedDifferByOne) {
+                        foreach(var existingToken in input) {
+                            if (DifferByOneCharacter(existingToken, token)) {
+                                matchedDifferByOne = true;
+                                commonDenominator = CommonDenominator(existingToken, token);
+                            }
+                        }
+                    }
+                    input.Add(token);
+                    if (ContainsExactly(token, 2)) {
+                        numberOfTwo++;
+                    }
+                    if (ContainsExactly(token, 3)) {
+                        numberOfThree++;
+                    }
+                } else {
+                    break;
+                }
+            } while (true);
+
+            Console.WriteLine(numberOfTwo + " x " + numberOfThree + " = " + (numberOfTwo * numberOfThree));
+            Console.WriteLine(commonDenominator);
+        }
+
+        private static bool ContainsExactly(string input, int expectedNumber) {
+            for (var outer = 0; outer < input.Length; outer++) {
+                var firstMatchIndex = input.IndexOf(input[outer]);
+                if (firstMatchIndex != -1 && firstMatchIndex < outer) {
+                    continue;
+                }
+                var numberOfMatches = 1;
+                for (var inner = outer+1; inner < input.Length; inner++) {
+                    if (input[outer] == input[inner]) {
+                        numberOfMatches++;
+                    }
+                }
+                if (expectedNumber == numberOfMatches) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool DifferByOneCharacter(string s1, string s2) {
+            var numberOfDifferentiatingCharacters = 0;
+            for (var index=0; index < s1.Length; index++) {
+                numberOfDifferentiatingCharacters += s1[index] != s2[index] ? 1 : 0;
+                if (numberOfDifferentiatingCharacters > 1) {
+                    return false;
+                }
+            }
+            return numberOfDifferentiatingCharacters == 1;
+        }
+
+        private static string CommonDenominator(string s1, string s2) {
+            string commonString = "";
+            for (var index=0; index < s1.Length; index++) {
+                commonString += s1[index] == s2[index] ? s1[index].ToString() : "";
+            }
+            return commonString;
+        }
+    }
+}
